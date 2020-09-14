2b0c2444262212e50805bcb8aa56fb72cc6c2487
Zvei Eliezer Nir <zveialiezer04@gmail.com>
2020-09-14 20:54:44 +0300

    Basic React
    After installing multiple dependencies, a navbar component was implemented including react routing, and a "create manager" form was written - currently it just prints the new object to console

--- mern-covid19-aid/src/App.js
+++ mern-covid19-aid/src/App.js
@@ -1,19 +1,42 @@
+/*
+* App.js
+* mern-covid19-aid project
+* Zvei Eliezer Nir & Refael Knoll
+*
+* main application logic, mainly Routes for CRUD operations to each model
+*
+*/
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import HomePage from "./components/home-page.component";
+import CreateManagers from "./components/create-managers.component";
+
+/*import ReadManagers from "./components/read-managers.component";
+import UpdateManagers from "./components/update-managers.component";
+import DeleteManagers from "./components/delete-managers.component";*/
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="container">
+          <Navbar />
+          <br />
+          <Route path="/" exact component={HomePage} />
+
+          <Route path="/managers-create/" component={CreateManagers} />
+          {/*
+          <Route path="/managers/read" component={ReadManagers} />
+          <Route path="/managers/update/:id" component={UpdateManagers} />
+          <Route path="/managers/delete/:id" component={DeleteManagers} />
+          */}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ mern-covid19-aid/src/components/create-managers.component.js
@@ -0,0 +1,181 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class CreateManagers extends Component {
+  constructor(props) {
+    super(props);
+    
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeFirstname = this.onChangeFirstname.bind(this);
+    this.onChangeLastname = this.onChangeLastname.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePhone = this.onChangePhone.bind(this);
+    this.onChangeCity = this.onChangeCity.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      password: '',
+      firstname: '',
+      lastname: '',
+      email: '',
+      phone: 0,
+      city: '',
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    console.log("mount")
+    this.setState({
+        users:  ['test user'],
+        username: 'test user'
+    })
+
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  onChangeFirstname(e) {
+    this.setState({
+      firstname: e.target.value
+    })
+  }
+
+  onChangeLastname(e) {
+    this.setState({
+      lastname: e.target.value
+    })
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  onChangePhone(e) {
+    this.setState({
+      phone: e.target.value
+    })
+  }
+
+  onChangeCity(e) {
+    this.setState({
+      city: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const manager = {
+      username: this.state.username,
+      password: this.state.password,
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      email: this.state.email,
+      phone: this.state.phone,
+      city: this.state.city
+    }
+
+    console.log(manager);
+
+    window.location = '/';
+  }
+
+  render() {
+    return (
+    <div>
+      <h3>Create New Manager</h3>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group"> 
+          <label>Username: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+              />
+        </div>
+        <div className="form-group"> 
+          <label>Password: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.password}
+              onChange={this.onChangePassword}
+              />
+        </div>
+        <div className="form-group">
+          <label>First Name</label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.firstname}
+              onChange={this.onChangeFirstname}
+              />
+        </div>
+
+        <div className="form-group">
+          <label>Last Name</label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.lastname}
+              onChange={this.onChangeLastname}
+              />
+        </div>
+
+        <div className="form-group">
+          <label>E-Mail</label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+              />
+        </div>
+
+        <div className="form-group">
+          <label>Phone Number</label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.phone}
+              onChange={this.onChangePhone}
+              />
+        </div>
+
+        <div className="form-group">
+          <label>City</label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.city}
+              onChange={this.onChangeCity}
+              />
+        </div>
+        
+
+        <div className="form-group">
+          <input type="submit" value="Create Manager" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+    )
+  }
+}
